c25b7a3ecd3ee01e5c49ab1a31e0fb41b9327ff5
Rafael Caetano Pinto <barenko@gmail.com>
2023-07-26 11:34:01 -0300

    feat(sensitive): Added sensitive filter information\n\nUse the params 'sensitiveHeaders', 'sensitiveResponseFields' and 'sensitiveRequestFields' to replace the value content by 'CONFIDENTIAL'

--- generate-payloads.js
+++ generate-payloads.js
@@ -1,21 +1,33 @@
-'use strict'
+//'use strict'
 
-const newman = require('newman');
-const fs = require('fs-extra');
+const newman = require('newman')
+const fs = require('fs-extra')
 const chalk = require( 'chalk' )
-const argv = require( 'minimist' )( process.argv.slice( 2 ) );
+const jp = require('jsonpath')
+const argv = require( 'minimist' )( process.argv.slice( 2 ) )
 
 const help = () => {
     console.log( 'Usage:' )
-    console.log( chalk.yellow( '\tnode generate-payloads.js [OPTIONS]\\' ) )
-    console.log( chalk.red( '\t\t--postman=/path/to/postman/collection.json \\' ) )
-    console.log( chalk.blue( '\t\t--output=/path/to/save \\' ) )
-    console.log( chalk.blue( '\t\t--posfix=posfix-name \\' ) )
-    console.log( 'Where:' )
-    console.log( `\t${chalk.red( 'postman' )}: path to postman collection. Required!` )
-    console.log( `\t${chalk.blue( 'output' )}: path to where the responses will be saved. Optional. Default: ./responses-<branch>` )
-    console.log( `\t${chalk.blue( 'posfix' )}: Folder name posfix. Optional` )
+    console.log( chalk.yellow( '\tnode generate-payloads.js \\' ) )
+    console.log( `\t\t[${chalk.red( '--postman' )}=/path/to/postman/collection.json] \\` )
+    console.log( `\t\t[${chalk.blue( '--output' )}=/path/to/save] \\` )
+    console.log( `\t\t[${chalk.blue( '--overwrite' )}] \\` )
+    console.log( `\t\t[${chalk.blue( '--sensitiveHeaders' )}=key1;key2] \\` )
+    console.log( `\t\t[${chalk.blue( '--sensitiveRequestFields' )}=key1;key2] \\` )
+    console.log( `\t\t[${chalk.blue( '--sensitiveResponseFields' )}=key1;key2] \\` )
+    console.log( '\nWhere:' )
+    console.log( `\t${chalk.red( 'postman' )}:   Path to postman collection. Required!` )
+    console.log( `\t${chalk.blue( 'output' )}:    Path to where the responses will be saved. Optional. Default: ./responses` )
     console.log( `\t${chalk.blue( 'overwrite' )}: Overwrite the output folder. Optional` )
+    console.log( `\t${chalk.blue( 'sensitiveHeaders' )}: List of headers to be censured in the output files, separated by comma. Optional. Default: Postman-Token` )
+    console.log( `\t${chalk.blue( 'sensitiveRequestFields' )}: List of payload fields to be censured in the output files, separated by comma. Each field must be described using jsonpath from 'request'. Optional. References: ${chalk.green('https://www.npmjs.com/package/jsonpath')}` )
+    console.log( `\t${chalk.blue( 'sensitiveResponseFields' )}: List of payload fields to be censured in the output files, separated by comma. Each field must be described using jsonpath from 'response'. Optional. References: ${chalk.green('https://www.npmjs.com/package/jsonpath')}` )
+    console.log( '\nExamples:' )
+    console.log(`\tnode generate-payloads.js --postman=MyApi.postman_collection.json\n`)
+    console.log(`\tnode generate-payloads.js --postman=MyApi.postman_collection.json --output=./api-output --overwrite\n`)
+    console.log(`\tnode generate-payloads.js --postman=MyApi.postman_collection.json --sensitiveHeaders=Cookie;Authorization\n`)
+    console.log(`\tnode generate-payloads.js --postman=MyApi.postman_collection.json --sensitiveResponseFields=$.access_token;$.data[*].customer.cpf[*];$.creation_date\n`)
+    console.log(`\tnode generate-payloads.js --postman ./ReclameAqui.postman_collection.json --sensitiveHeaders=Authorization,Cookie --sensitiveResponseFields='$..access_token;$..creation_date;'\n`)
 }
 
 if( argv.h || argv.help ){
@@ -30,7 +42,10 @@ if( argv.h || argv.help ){
 
 // Replace 'path/to/your/postman_collection.json' with the actual path to your Postman collection JSON file
 const collectionFile = argv.postman;
-const outputFolder = argv.output || 'responses'+(argv.posfix && '-'+argv.posfix || '');
+const sensitiveHeaders = [...argv.sensitiveHeaders?.split(/\s*[;,:]\s*/)??[], 'Postman-Token']
+const sensitiveRequestFields = argv.sensitiveRequestFields?.split(/\s*[;,:]\s*/) ?? []
+const sensitiveResponseFields = argv.sensitiveResponseFields?.split(/\s*[;,:]\s*/) ?? []
+const outputFolder = argv.output || 'responses';
 
 if(argv.overwrite){
     fs.rmSync(outputFolder, { recursive: true, force: true });
@@ -48,22 +63,33 @@ newman.run({
     const folderName = collectionFile.replace(/.*\b([^.]+).postman_collection.json/, '$1').replace(/[^a-z0-9]/gi, '_').toLowerCase();
     const fileName = `${folderName}.${args.item.name.replace(/[^a-z0-9]/gi, '_').toLowerCase()}.json`;
 
-    const body = args.request?.body?.toString();
+    args.request.headers.members.map(h => {
+        if(sensitiveHeaders.includes(h.key) && h.value){
+            h.value = 'CONFIDENTIAL'
+        }
+    })
 
-    const headersMembers = args.request.headers.members.filter(member => member.key !== 'Postman-Token');
-    args.request.headers.members = headersMembers;
+    const requestBody = JSON.parse(args.request?.body?.toString()||'{}')
+    const responseBody = JSON.parse(args.response.stream.toString()||'{}')
+
+    sensitiveRequestFields.forEach((i)=>{
+        jp.value(requestBody, i, 'CONFIDENTIAL')
+    })
+    sensitiveResponseFields.forEach((i)=>{
+        jp.value(responseBody, i, 'CONFIDENTIAL')
+    })
 
     const response = {
         request: {
             method: args.request.method,
             url: args.request.url.toString(),
             headers: args.request.headers,
-            body: body && JSON.parse(body.toString()),
+            body: requestBody,
         },
 
         response: {
             code: args.response.code,
-            body: JSON.parse(args.response.stream.toString()),
+            body: responseBody,
         },
     };
 
